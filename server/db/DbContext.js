1b21f2e80d81230481f33755615b4af91300e911
Steffen Larson <steffman13@gmail.com>
2021-01-15 15:08:15 -0700

    removed user from dbcontext

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose'
 import AccountSchema from '../models/Account'
-import UsersSchema from '../models/User'
 import CaptionImagesSchema from '../models/CaptionImage'
 import CaptionStringsSchema from '../models/CaptionString'
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
-  Users = mongoose.model('User', UsersSchema);
   CaptionImages = mongoose.model('CaptionImage', CaptionImagesSchema);
   CaptionStrings = mongoose.model('CaptionString', CaptionStringsSchema)
 }
