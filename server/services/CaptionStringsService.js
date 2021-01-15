94ca268eef6e447c1813f0269b65417b68959d7b
Steffen Larson <steffman13@gmail.com>
2021-01-15 13:19:51 -0700

    CaptionString Service

---
+++ server/services/CaptionStringsService.js
@@ -0,0 +1,39 @@
+import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
+
+class CaptionStringsService {
+  async find(query = {}) {
+    const captionString = await dbContext.CaptionStrings.find(query).populate('user')
+    return captionString
+  }
+
+  async findById(id) {
+    const captionstring = await dbContext.CaptionStrings.findById(id)
+    if (!captionstring) {
+      throw new BadRequest('Invalid Id')
+    }
+    return captionstring
+  }
+
+  async create(body) {
+    return await dbContext.CaptionStrings.create(body)
+  }
+
+  async edit(captionString) {
+    const newCaptionString = await dbContext.CaptionStrings.findOneAndUpdate({ _id: captionString.id, submittedUser: captionString.submittedUser }, captionString, { new: true }).populate('user')
+    if (!newCaptionString) {
+      throw new BadRequest('You are not the creator, or this is not a valid Caption String')
+    }
+    return newCaptionString
+  }
+
+  async delete(id, userId) {
+    const captionString = await dbContext.CaptionStrings.findOneAndRemove({ _id: id, submittedUser: userId })
+    if (!captionString) {
+      throw new BadRequest('You are not the user, or this is not a valid Caption String')
+    }
+    return 'successfully deleted'
+  }
+}
+
+export const captionStringsService = new CaptionStringsService()
