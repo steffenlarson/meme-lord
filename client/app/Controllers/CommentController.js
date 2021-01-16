b2414efc29e681952abfcca746e455005382e5c3
RyanDavis-bcw <mister.wizard21@gmail.com>
2021-01-15 21:18:27 -0700

    fixes formatting

--- client/app/Controllers/CommentController.js
+++ client/app/Controllers/CommentController.js
@@ -6,7 +6,7 @@ function _draw() {
 
   posts.forEach(i => {
     let template = ''
-    i.comments.forEach(c => {
+    i.captions.forEach(c => {
       template += c.Template
     })
     document.getElementById(`_${i.id}`).innerHTML = template
