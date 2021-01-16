6d9df2b564d6c92b4e4d433102f057e06ea9f7f0
RyanDavis-bcw <mister.wizard21@gmail.com>
2021-01-15 19:32:14 -0700

    fixes collapse element

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -13,14 +13,14 @@ export default class Post {
   get Template() {
     return `
          <div class="card" style="width: 18rem;">
-          <img src="${this.imgUrl}" class="card-img-top" alt="">
+          <img src="${this.imgUrl}" class="card-img-top img-fluid" alt="">
           <form onsubmit="app.commentController.create()">
             <input type="text" name="newComment" id="" maxlength="50" onsubmit="app.captionStringController.create()">
             <button class="btn btn-danger" type="button" aria-expanded="false" aria-controls="commentField"
-              data-bs-toggle="collapse" onclick="app.postController.getcomments('${this.id}')" data-bs-target="#${this.id}">Show Comments</button>
+              data-bs-toggle="collapse" onclick="app.postController.getcomments('${this.id}')" data-bs-target="#_${this.id}">Show Comments</button>
           </form>
         </div>
-        <div class="collapse" id="${this.id}" style="min-height: 20vh;">
+        <div class="collapse" id="_${this.id}" style="min-height: 20vh;">
         
         </div>
 
