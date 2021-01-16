537f0923571cf6706e4da6c392e68d9713639f7e
Tim Zaleski <zaleskitimothy@yahoo.com>
2021-01-16 00:53:26 -0700

    important message

--- client/app/Controllers/UserController.js
+++ client/app/Controllers/UserController.js
@@ -4,6 +4,7 @@ import { userService } from "../Services/UserService.js"
 function _draw() {
   let users = ProxyState.users;
   let template = ''
+  // ??????????????????? - Tim
   users.forEach(u => {
     template += u.Template
   })
