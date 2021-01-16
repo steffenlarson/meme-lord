b068108af68de12dab278225217fdd9ddd08c7f8
RyanDavis-bcw <mister.wizard21@gmail.com>
2021-01-15 17:45:23 -0700

    postservice

--- client/app/Services/PostService.js
+++ client/app/Services/PostService.js
@@ -5,19 +5,19 @@ import { memeapi } from "../Services/AxiosService.js"
 class PostService {
 
   async create(newImg) {
-    await memeapi.post('captionimages', newImg)
+    await memeapi.post('api/captionimages', newImg)
     let img = new Post(newImg)
     ProxyState.posts = [...ProxyState.posts, img]
   }
   async getcomments(id) {
-    let res = await memeapi.get('captionimages/' + id + "/captionstrings")
+    let res = await memeapi.get('api/captionimages/' + id + "/captionstrings")
     let current = ProxyState.posts.find(p => p.id == id)
     console.log(res.data)
     current.comments = [res.data]
   }
 
   async loadPosts() {
-    let res = await memeapi.get("captionimages")
+    let res = await memeapi.get("api/captionimages")
     console.log(res.data)
     ProxyState.posts = [res.data]
   }
